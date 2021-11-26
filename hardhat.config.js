82df3a801d4a84e9e1201c8720e65df34ae4139f
Victor Lopez <victor.98.javier@gamil.com>
2021-11-26 17:20:45 -0500

    the nfts now have cost. and there is a funtion to witdraw ether.

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
 require('dotenv').config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
@@ -25,4 +26,9 @@ module.exports = {
       accounts:[process.env.PRIVATE_KEY],
     },
   },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: process.env.ETHERSCAN_KEY,
+  }
 };
